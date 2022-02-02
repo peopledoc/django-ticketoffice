b0a8ab76661077103d603eec1704e842a1b0cb98
Florent PIGOUT <florent.pigout@people-doc.com>
2022-02-02 09:24:18 +0100

    Add django3 support

--- django_ticketoffice/tests.py
+++ django_ticketoffice/tests.py
@@ -36,7 +36,7 @@ class TicketModelTestCase(django.test.TestCase):
         """Ticket.generate_password return random (clear) password."""
         ticket = models.Ticket()
         self.assertTrue(is_valid_password(ticket.password))
-        generate_password_mock = mock.Mock(return_value=mock.sentinel.password)
+        generate_password_mock = mock.Mock(return_value='a-password')
         with mock.patch('django_ticketoffice.utils.random_password',
                         new=generate_password_mock):
             password = ticket.generate_password()
@@ -44,7 +44,7 @@ class TicketModelTestCase(django.test.TestCase):
         generate_password_mock.assert_called_once_with(
             *TICKETOFFICE_PASSWORD_GENERATOR[1],
             **TICKETOFFICE_PASSWORD_GENERATOR[2])
-        self.assertEqual(password, mock.sentinel.password)
+        self.assertEqual(password, 'a-password')
 
 
 class TicketManagerTestCase(django.test.TestCase):
